f1b531f405fbe6a156484723b2e6ac930514af46
Fabien Fernandes Alves <fwfabou@gmail.com>
2024-09-19 16:03:51 -0400

    feat: finish chapter 2 about integers

---
+++ learn-with-tests/integers/add_test.go
@@ -0,0 +1,23 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	t.Run("add two numbers", func(t *testing.T) {
+		sum := Add(1, 2)
+		expected := 3
+
+		if (sum != expected) {
+			t.Errorf("got %d want %d", sum, expected)
+		}
+	})
+}
+
+func ExampleAdd() {
+	sum := Add(1, 2)
+	fmt.Println(sum)
+	// Output: 3
+}
